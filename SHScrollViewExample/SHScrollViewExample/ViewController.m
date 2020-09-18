4af796a82de79cfc12e3861f6bae23a468a187c3
CCSH <624089195@qq.com>
2020-09-18 16:57:43 +0800

    update 优化计算

--- SHScrollViewExample/SHScrollViewExample/ViewController.m
+++ SHScrollViewExample/SHScrollViewExample/ViewController.m
@@ -35,11 +35,9 @@
     NSArray *imageArr = @[@"http://txt25-2.book118.com/2017/0420/book99787/99786555.jpg",
                           @"http://pic23.nipic.com/20120816/10691507_162853344170_2.jpg",
                           lab,
-                          @"http://pic.58pic.com/58pic/15/63/07/42Q58PIC42U_1024.jpg",
                           @"http://d.hiphotos.baidu.com/image/h%3D200/sign=8663264274f082023292963f7bfbfb8a/f3d3572c11dfa9eca13b947665d0f703918fc1be.jpg",
                           lab2,
                           @"http://img.zcool.cn/community/01690955496f930000019ae92f3a4e.jpg@2o.jpg"];
-    
     
     SHScrollView *view = [[SHScrollView alloc]init];
 //    view.frame = CGRectMake(0, 100, self.view.frame.size.width, 200);
